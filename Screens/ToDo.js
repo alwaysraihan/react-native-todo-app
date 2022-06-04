8bd772509060f1d101263f00b863dbb40413f8ee
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-04 16:31:00 +0600

    Todo Task Text Input, ADD text and Clear text added and styling implemented

--- Screens/ToDo.js
+++ Screens/ToDo.js
@@ -1,13 +1,18 @@
+import React, { useState } from "react";
 import {
     StyleSheet,
     Text,
     View,
     ImageBackground,
     TextInput,
+    TouchableOpacity,
 } from "react-native";
-import React from "react";
 
 const Notes = () => {
+    const [newTask, setNewTask] = useState("");
+    const addNewTask = (enteredTesxt) => {
+        setNewTask(enteredTesxt);
+    };
     return (
         <View style={styles.container}>
             <ImageBackground
@@ -15,8 +20,31 @@ const Notes = () => {
                 resizeMode="cover"
                 style={{ flex: 1 }}
             >
-                <View>
-                    <TextInput></TextInput>
+                <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                >
+                    <TextInput
+                        style={styles.inputBox}
+                        placeholder="Enter a new task"
+                        onChange={addNewTask}
+                    />
+                </View>
+                <View
+                    style={{
+                        flexDirection: "row",
+
+                        width: "80%",
+                        marginTop: 30,
+                        marginHorizontal: "auto",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <TouchableOpacity style={styles.butonDesign}>
+                        <Text style={styles.buttonText}>Clear</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.butonDesign}>
+                        <Text style={styles.buttonText}>Add</Text>
+                    </TouchableOpacity>
                 </View>
             </ImageBackground>
         </View>
@@ -26,6 +54,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
+    },
+    inputBox: {
+        width: "80%",
+        height: 50,
+        borderWidth: 5,
+        borderColor: "rgba(230,191,131,0.7))",
+        marginTop: 100,
+        fontSize: 25,
+        paddingHorizontal: 10,
+    },
+    butonDesign: {
+        backgroundColor: "rgba(230,191,131,0.3)",
+        width: 80,
+        height: 40,
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 20,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonText: {
+        fontSize: 25,
+        textAlign: "center",
     },
 });
 export default Notes;
