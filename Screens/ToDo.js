f2e1a5e9f0ce3bca4456c94266be83b4775ca3bd
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-04 15:49:20 +0600

    react navigration setup

---
+++ Screens/ToDo.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Notes = () => {
+    return (
+        <View>
+            <Text>Notes</Text>
+        </View>
+    );
+};
+
+export default Notes;
